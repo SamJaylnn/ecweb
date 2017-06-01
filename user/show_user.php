1bb73d8317ffe7f5e5d973117d042b20fc76b696
SamJaylnn <chenshuzhongs@gmail.com>
2017-06-01 22:36:24 +0000

    file system structure modify

--- user/show_user.php
+++ user/show_user.php
@@ -1,6 +1,27 @@
 <?php
-define('__ROOT__', dirname(dirname(__FILE__)));
-require_once(__ROOT__.'/resources/database.php'); 
+    function mysqliConnect($query) {
+        $connect = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "bb322e8bfccea9", "245af7a1", "heroku_4314e8da0b6f03d");  
+        $result = mysqli_query($connect, $query);
+        return $result;
+    }
+
+    function mysqlConnect($query) {
+        // Connect to MySQL
+        if ( !( $database = mysql_connect( "us-cdbr-iron-east-03.cleardb.net", "bb322e8bfccea9", "245af7a1") ) )
+            die( "Could not connect to database" );
+            
+        // open database
+        if ( !mysql_select_db( "heroku_4314e8da0b6f03d", $database ) )
+            die( "Could not open ecweb database" );
+            
+        // query ecweb database
+        if ( !( $result = mysql_query( $query, $database ) ) ) {
+            print( "Could not execute query! <br/>" );
+            die( mysql_error() );
+        }
+        mysql_close( $database );
+        return $result;
+    }
 ?>
    
 
